3c9a1ce218fb536bc12fdfb45e95c60963106f7f
leticiasoaress <leticiamoraiss@outlook.com>
2021-10-03 12:34:16 -0300

    Migrations

---
+++ AnotaAi.Repositorio/Migrations/20211003151732_BancoAnotaAiProducao.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnotaAi.Repositorio.Migrations
+{
+    public partial class BancoAnotaAiProducao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
